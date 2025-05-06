72afa2ef4a162e608a78c59a244cb17f3231f07e
that-toaster-boyo <117860729+thatboyo@users.noreply.github.com>
2025-05-06 09:06:32 -0700

    Create discord.py

---
+++ discord.py
@@ -0,0 +1,10 @@
+import Discord
+client = discordClient()
+#connects toast bot to discord
+@client.event
+async def on_ready(): #confirm bot online
+    print(f'toaster bot online!')
+
+
+@client.event
+async def 
